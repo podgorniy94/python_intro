46498980ae88b0060e0bb06ed5f4c5563fb92491
Rostyslav Podgorniy <podgorniy.inc@gail.com>
2021-11-04 18:52:04 +0100

    Added print function for output

--- python_data_types.py
+++ python_data_types.py
@@ -1,5 +1,5 @@
-1256983%28 #task on page 10
+print (1256983%28) #task on page 10
 
-12**52/15>8 or 3**5>100 #task on page 16
+print (12**52/15>8 or 3**5>100) #task on page 16
 
-not(5*3**3==900/75) # task on page 16
+print (not(5*3**3==900/75)) # task on page 16
