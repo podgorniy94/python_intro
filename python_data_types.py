e27f5ca5aa83e3b93581f23f60e611bb95dbfadc
podgorniy94 <podgorniy.inc@gmail.com>
2021-11-05 18:34:02 +0100

    Re-changing the layout of comments.

--- python_data_types.py
+++ python_data_types.py
@@ -1,5 +1,6 @@
-print (1256983%28) # task on page 10
+# task on page 10
+print (1256983%28) 
 
-print (12**52/15>8 or 3**5>100) # task on page 16
-
-print (not(5*3**3==900/75)) # task on page 16
+# task on page 16
+print (12**52/15>8 or 3**5>100) # exercise 1
+print (not(5*3**3==900/75)) # exercise 2
