469a10d49e94f799eef0b7fc99a45cca9c3dc5fb
Rostyslav Podgorniy <podgorniy.inc@gmail.com>
2021-11-11 00:07:40 +0100

    Completed the first exercise on page 12 (list)

---
+++ python_loops.py
@@ -0,0 +1,9 @@
+# Exercie on page 12 (Seznamy)
+
+names_list = ['Jiri', 'Jan', 'Marie', 'Petr', 'Jana', 'Josef','Pavel', 'Martin', 'Jaroslav', 'Tomas', 'Eva', 'Miroslav', 'Hana', 'Anna', 'Zdenek', 'Frantisek', 'Vaclav', 'Michal', 'Lenka', 'Katerina']
+name = input('Type your name ')
+
+if  (name in names_list) == True:
+  print('Indetification passed')
+else:
+  print('Indetification failed')
