04d3c77b3a38db893f0a107f2da97558f065f114
Rostyslav Podgorniy <podgorniy.inc@gmail.com>
2021-11-14 20:09:33 +0100

    2 more exercises solved (List Comprehension, GATTACA)

--- python_loops.py
+++ python_loops.py
@@ -9,7 +9,6 @@ else:
 
 # Exercie on page 20 (Smyčky)
 d = {'a':'alfa', 'b':'bravo', 'c':'charlie', 'd':'delta', 'e':'echo', 'f':'foxtrot', 'g':'golf', 'h':'hotel', 'i':'india', 'j':'juliett', 'k':'kilo', 'l':'lima', 'm':'mike', 'n':'november', 'o':'oscar', 'p':'papa', 'q':'quebec', 'r':'romeo', 's':'sierra', 't':'tango', 'u':'uniform', 'v':'victor', 'w':'whiskey', 'x':'x-ray', 'y':'yankee', 'z':'zulu'}
-
 name = input('Tape your name: ')
 name = name.lower()
 
@@ -37,3 +36,19 @@ for i, search in enumerate(items):
         items.append(item)
         print(items)
         break
+
+# Exercise on page 28 (List comprehension)
+numbers = range(5,10)
+exp = [x*x for x in numbers]
+fav = [str(x) + ' is my favorite number!' for x in numbers]
+print(exp), print(fav)
+
+# Exercise on page 29 (GATTACA)
+seq = 'ACTGCTCAAG'
+inlist = [i for i, x in enumerate(seq) if x == 'A']
+print(inlist)
+# The second solution
+seq = 'ACTGCTCAAG'
+no_enum = [i for i in range(len(seq)) if seq[i] == 'A']
+print(no_enum)
+
