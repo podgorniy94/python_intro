1bdc33e894264498aadc720ba12d1764bdfa17af
Rostyslav Podgorniy <podgorniy.inc@gmail.com>
2021-11-14 01:03:00 +0100

    Completed two new exercises loops and transpose matrix

--- python_loops.py
+++ python_loops.py
@@ -7,3 +7,20 @@ if  (name in names_list) == True:
     print('Indetification passed')
 else:
     print('Indetification failed')
+
+# Exercie on page 20 (Smyčky)
+d = {'a':'alfa', 'b':'bravo', 'c':'charlie', 'd':'delta', 'e':'echo', 'f':'foxtrot', 'g':'golf', 'h':'hotel', 'i':'india', 'j':'juliett', 'k':'kilo', 'l':'lima', 'm':'mike', 'n':'november', 'o':'oscar', 'p':'papa', 'q':'quebec', 'r':'romeo', 's':'sierra', 't':'tango', 'u':'uniform', 'v':'victor', 'w':'whiskey', 'x':'x-ray', 'y':'yankee', 'z':'zulu'}
+
+name = input('Tape your name: ')
+name = name.lower()
+
+for letters in name:
+    print(letters + ' = ' + d[letters])
+
+# Exercise on page 21 (Trasnponovaná matice)
+# new_list = [expression for member in iterable]
+matrix = [[1,2,3], [4,5,6], [7,8,9]]
+
+transpose = [[row[i] for row in matrix] for i in range(len(matrix))]
+for lst in transpose:
+        print(lst)
