6dda54cb4b0146163e7b684bd38be9f494cbee1b
Rostyslav Podgorniy <podgorniy.inc@gmail.com>
2021-11-16 22:36:55 +0100

    Done second exercise with Lambda function

--- python_functions.py
+++ python_functions.py
@@ -6,9 +6,13 @@ print(division())
 # exercise 2
 def lst_sum():
     '''I had to look for tips on how to get input separated by spaces. I chose the string split () method.'''
-    lst_num = [int(elements) for elements in input('Type list of numbers: ').split()]
+    lst_num = [int(elements) for elements in input('Type list of numbers separated by space: ').split()]
     total = 0
     for num in lst_num:
         total += num
     print(total)
 lst_sum()
+
+# Lambda funkce (12)
+compare_list = (lambda lst: print('small list') if len(lst) <= 5 else print('big list'))
+compare_list([int(nmbrs) for nmbrs in input('Type list of numbers separated by space ').split()])
